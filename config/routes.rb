a966988200ce01234f6657cbf5417ad78a2090f8
Stephanie Barnet <slbarnet@gmail.com>
2015-03-01 17:38:58 +0000

    added css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  get '/about_us' => "welcome#about_us"
+  
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
